fcb318f99bb931bbaab2cb7228b5f50b35bd2a51
Administrator <Administrator@DELL>
2020-06-26 20:05:04 +0530

    Project guide file is added.

--- SpringCoreBeanScopeDemo/src/main/java/com/javaLive/main/App.java
+++ SpringCoreBeanScopeDemo/src/main/java/com/javaLive/main/App.java
@@ -12,8 +12,25 @@ import com.javaLive.beans.SingletonBean;
  * This programs demonstrates scopes of Spring beans.
  * Singleton scopes bean refers to the same instance while prototype creates new instance for each time
  * when created each time.
- * The instances can be checked using == i.e. equals operator. 
- *
+ * The instances can be checked using == i.e. equals operator.
+ * 
+ * Above are scopes at core level and we are only explaining them here.
+ * Below are the scopes at web level i.e. they are applicable for web based spring projects.
+ * We will study them in web based spring project.
+ *   
+ * Spring bean scopes applicable for web based project are as follows:
+ * 
+ * request-  Spring bean configured as request scope instantiates the bean for a single HTTP request. 
+ * Since HTTP requests are available only in web applications this scope is valid only for web-aware spring 
+ * application context.
+ * 
+ * session – Spring Beans configured as session scope lives through the HTTP session. Similar to request scope,
+ * it is applicable only for web aware spring application contexts.
+ * 
+ * global_session- This scope is equal to the  session scope on portlet applications. 
+ * This scope is also applicable only for web aware spring application contexts. 
+ * If this is global_session is used in normal web application (not in portlet), 
+ * then it will behave as session scope and there will not be any error.
  */
 public class App {
 	public static void main(String[] args) {
